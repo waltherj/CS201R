cced82a37f7c2e329b8d3f78d599f4cc2599f8d8
waltherj <waltherj@byu.net>
2017-01-25 02:13:52 -0700

    Finishing up for tonight. The snake moves around the screen as a one-celled organism

--- snake/snake.js
+++ snake/snake.js
@@ -6,16 +6,57 @@ var GridEnum = {
 	FOOD: 2,
 	EMPTY: 3
 };
+var DirEnum = {
+	LEFT:1,
+	RIGHT:2,
+	UP:3,
+	DOWN:4
+};
 
+var SNAKE = {
+	x : 0,y : 0
+}
+var HEIGHT = 8;
+var WIDTH = 8;
+
+function moveSnake(direction) {
+	gameArray[SNAKE.y][SNAKE.x] = GridEnum.EMPTY;
+	if (direction == DirEnum.LEFT) {
+		SNAKE.x--;
+	} 
+	else if (direction == DirEnum.RIGHT) {
+		SNAKE.x++;
+	} 
+	else if (direction == DirEnum.UP) {
+		SNAKE.y--;
+	} 
+	else if (direction == DirEnum.DOWN) {
+		SNAKE.y++;
+	}  
+	if (SNAKE.x >= WIDTH) {
+		SNAKE.x -= WIDTH;
+	}
+	if (SNAKE.y >= HEIGHT) {
+		SNAKE.y -= HEIGHT;
+	}
+	if (SNAKE.x < 0) {
+		SNAKE.x += WIDTH;
+	}
+	if (SNAKE.y < 0) {
+		SNAKE.y += HEIGHT;
+	}
+	gameArray[SNAKE.y][SNAKE.x] = GridEnum.SNAKE;
+}
 function convertGameToDisplay() {
+
 	var i,j;
 	for (i = 0; i < 8; i++) {
 		for (j = 0; j < 8; j++) {
 			if (gameArray[i][j] == GridEnum.EMPTY) {
-				displayArray[i][j] = "0";
+				displayArray[i][j] = " ";
 			}
 			else if (gameArray[i][j] == GridEnum.SNAKE) {
-				displayArray[i][j] = "|";
+				displayArray[i][j] = "0";
 			}
 			else if (gameArray[step] == GridEnum.FOOD) {
 				displayArray[i][j] = "*";
@@ -40,7 +81,7 @@ function createTable(tableData) {
     tableBody.appendChild(row);
   });
   table.appendChild(tableBody);
-  document.getElementById("gameFrame").appendChild(table);
+  document.getElementById("gameTable").innerHTML = table.innerHTML;
 }
 
 var size = 8;
@@ -54,3 +95,23 @@ while (size--) {
 }
 convertGameToDisplay();
 createTable(displayArray);
+
+function run() {
+setInterval(function() { eternalSnake() }, 100);
+}
+
+var w = 1;
+function eternalSnake() {
+if (w > 0) {
+moveSnake(DirEnum.DOWN);
+}
+else {
+moveSnake(DirEnum.LEFT);
+}
+w = w * -1;
+convertGameToDisplay();
+createTable(displayArray);
+
+}
+
+window.onload = run;
