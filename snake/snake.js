2866496bab224e8d3796746352f1d301470568b7
waltherj <waltherj@byu.net>
2017-01-25 01:36:59 -0700

    Getting the basic pieces together

--- snake/snake.js
+++ snake/snake.js
@@ -1,2 +1,56 @@
 //Alphabet Snake
 //By J.R. Walther & Konnor Petersen
+
+var GridEnum = {
+	SNAKE: 1,
+	FOOD: 2,
+	EMPTY: 3
+};
+
+function convertGameToDisplay() {
+	var i,j;
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			if (gameArray[i][j] == GridEnum.EMPTY) {
+				displayArray[i][j] = "0";
+			}
+			else if (gameArray[i][j] == GridEnum.SNAKE) {
+				displayArray[i][j] = "|";
+			}
+			else if (gameArray[step] == GridEnum.FOOD) {
+				displayArray[i][j] = "*";
+			}
+		}
+	}
+}
+
+function createTable(tableData) {
+  var table = document.createElement('table');
+  var tableBody = document.createElement('tbody');
+
+  tableData.forEach(function(rowData) {
+    var row = document.createElement('tr');
+
+    rowData.forEach(function(cellData) {
+      var cell = document.createElement('td');
+      cell.appendChild(document.createTextNode(cellData));
+      row.appendChild(cell);
+    });
+
+    tableBody.appendChild(row);
+  });
+  table.appendChild(tableBody);
+  document.getElementById("gameFrame").appendChild(table);
+}
+
+var size = 8;
+var gameArray = new Array(size);
+var displayArray = new Array(size);
+while (size--) {
+	var length = 8;
+	gameArray[size] = new Array(length);
+	displayArray[size] = new Array(length);
+	while(length--) gameArray[size][length] = GridEnum.EMPTY;
+}
+convertGameToDisplay();
+createTable(displayArray);
