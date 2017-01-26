d2b4467ae6eda9e90f59b8207331bc47bb86c093
Konnor Petersen <konnorjp@gmail.com>
2017-01-25 17:13:53 -0700

    Prevent scrolling with arrows

--- snake/snake.js
+++ snake/snake.js
@@ -181,3 +181,9 @@ case 39:
     break;
 }
 }
+
+window.addEventListener("keydown", function(e) {
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+    e.preventDefault();
+    }
+    }, false);
